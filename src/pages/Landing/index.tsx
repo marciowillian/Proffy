da6959dbd7043d923635b4f51c6c9e95a3524524
Marcio Willian <willian_mw.cc@hotmail.com>
2020-08-08 23:11:09 -0300

    M - Concluindo css da pagina inicial

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-import lstudyIcon from '../../assets/images/icons/study.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing() {
     return (
@@ -23,13 +26,13 @@ function Landing() {
 
                 <div className="buttons-container">
                     <a href="" className="study">
-                        <img src={lstudyIcon} alt="Estudar"/>
+                        <img src={studyIcon} alt="Estudar"/>
                         Estudar
                     </a>
 
                     <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                        Estudar
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
                     </a>
                 </div>
 
