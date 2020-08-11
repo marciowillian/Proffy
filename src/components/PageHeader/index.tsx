bea727e1e50a86fc9a6715dabf82f62def393572
Marcio Willian <willian_mw.cc@hotmail.com>
2020-08-11 01:27:49 -0300

    D - Estilizando css da pagina de listagem de proffys

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -12,17 +12,19 @@ interface PageHeaderProps {
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar" />
+                </Link>
+                <img src={logoImg} alt="Proffy" />
+            </div>
 
-                <div className="header-content">
-                    <strong>{props.title}</strong>
-                </div>
-            </header>
+            <div className="header-content">
+                <strong>{props.title}</strong>
+
+                {props.children}
+            </div>
+        </header>
     )
 }
 
